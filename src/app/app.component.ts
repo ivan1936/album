3b980c42d226642d281cafb4711d6a74d4140d71
ivan1936 <qaz19361@mail.ru>
2020-12-21 20:24:25 +0300

    two commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+
+import {CURRENT_PAGE, SEARCH_STRING} from 'src/app/shared/constants'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'album';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    localStorage.setItem(CURRENT_PAGE, JSON.stringify(1))
+    localStorage.setItem(SEARCH_STRING, JSON.stringify('alma ata'))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {AppRoutingModule} from './app-routing.module'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {NgxsModule} from '@ngxs/store'
+import {FlexLayoutModule} from '@angular/flex-layout'
+import {HttpClientModule} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component'
+import {environment} from './../environments/environment'
+import {MaterialModule} from './shared/modules/material.module'
+import {Pagination} from './shared/components/pagination/pagination.component'
+import {PhotoComponent} from './components/photo/photo.component'
+import {NotfoundComponent} from './shared/components/notfound/notfound.component'
+import {PhotosComponent} from './components/photos/photos.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagination,
+    PhotoComponent,
+    NotfoundComponent,
+    PhotosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
